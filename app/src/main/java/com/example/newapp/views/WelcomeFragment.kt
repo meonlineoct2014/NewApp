703a42e4097bdc594ff3f8ee954455917087c682
Ashutosh.D <Ashutosh.D@A4G516-PC.afourtech.pune>
2021-12-21 20:59:24 +0530

    added back & forward navigation and passed data using safearg plugin

--- app/src/main/java/com/example/newapp/views/WelcomeFragment.kt
+++ app/src/main/java/com/example/newapp/views/WelcomeFragment.kt
@@ -6,27 +6,31 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.example.newapp.R
-import com.example.newapp.databinding.FragmentHomeBinding
 import com.example.newapp.databinding.FragmentLoginBinding
+import com.example.newapp.databinding.FragmentWelcomeBinding
 
-class LoginFragment : Fragment(R.layout.fragment_login){
-    private var _binding: FragmentLoginBinding? = null
+class WelcomeFragment : Fragment(R.layout.fragment_welcome) {
+    private val args: WelcomeFragmentArgs by navArgs()
+
+    private var _binding: FragmentWelcomeBinding? = null
     private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        _binding = FragmentWelcomeBinding.inflate(inflater, container, false)
         val view = binding.root
-        binding.buttonSignin.setOnClickListener {
-            val username = binding.editTextUsername.text.toString()
-            val password = binding.editTextTextPassword.text.toString()
-
+        binding.textUserNameWelcome.text = args.username
+        binding.textPasswordWelcome.text = args.password
+        binding.buttonConfirm.setOnClickListener {
             findNavController().
-            navigate(LoginFragmentDirections.actionLoginFragmentToWelcomeFragment(username, password))
+            navigate(WelcomeFragmentDirections.actionWelcomeFragmentToHomeFragment())
         }
         return view
     }
+
 }
